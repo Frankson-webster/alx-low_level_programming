893361fdee39deaedce40c4dc40f1ac92558270d
Frankson-webster <franksonwebster@yahoo.com>
2023-02-22 18:04:21 -0800

    a function that prints all natural numbers from n to 98, followed by a new line.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print n to 98 counts
+ *           separated by comma, followed
+ *           by space and number should be
+ *           printed in order
+ *
+ * @n: input
+*/
+
+void print_to_98(int n)
+{
+	int webster;
+
+	if (n > 98)
+		for (webster = n; webster > 98; --webster)
+			printf("%d,", webster);
+	else
+		for (webster = n; webster < 98; ++webster)
+			printf("%d,", webster);
+	printf("98\n");
+}
